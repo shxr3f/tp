83b025b9bbb6c47816000dbc9292eefa6ba769fd
shxr3f <e0540641@u.nus.edu>
2022-03-23 17:14:48 +0800

    Trying to figure out what went wrong part 2

--- src/test/java/seedu/duke/helper/CommandTest.java
+++ src/test/java/seedu/duke/helper/CommandTest.java
@@ -45,45 +45,45 @@ class CommandTest {
     @Test
     public void viewPatientTest() {
         command.viewPatient(patients,"S1234567A");
-        assertEquals("HalpMi: There is no such patient\r\n",outContent.toString());
+        assertEquals("HalpMi: There is no such patient\n",outContent.toString());
     }
 
     @Test
     public void deletePatientTest() {
         String userInput = "S1234567A";
         command.deletePatient(patients,userInput);
-        assertEquals("HalpMi: Please input a positive number up to 0 only.\r\n"
-                + "        Here is an example:\r\n" + "        delete patient /info 1\r\n", outContent.toString());
+        assertEquals("HalpMi: Please input a positive number up to 0 only.\n"
+                + "        Here is an example:\n" + "        delete patient /info 1\n", outContent.toString());
     }
 
     @Test
     public void addDoctorTest() {
         String userInput = "S1234567A, John Doe, 23, M, 10 Baker Street, 1999-12-31, Emergency Medicine";
         command.addDoctor(doctors,userInput);
-        assertEquals("HalpMi: The doctor above has been added.\r\n",outContent.toString());
+        assertEquals("HalpMi: The doctor above has been added.\n",outContent.toString());
     }
 
     @Test
     public void deleteDoctorTest() {
         command.deleteDoctor(doctors,"");
-        assertEquals("HalpMi: Please input a positive number up to 0 only.\r\n"
-                + "        Here is an example:\r\n" + "        delete doctor /info 1\r\n",outContent.toString());
+        assertEquals("HalpMi: Please input a positive number up to 0 only.\n"
+                + "        Here is an example:\n" + "        delete doctor /info 1\n",outContent.toString());
     }
 
     @Test
     public void addMedicineTest() {
         String userInput = "Paracetamol";
         command.addMedicine(medicines,userInput);
-        assertEquals("HalpMi: There is one or more parameters missing\r\n"
-                + "HalpMi: Invalid format. Please follow the below example and try again.\r\n"
-                + "        add medicine /info Paracetamol,500,2023-12-12,Headaches,10\r\n",outContent.toString());
+        assertEquals("HalpMi: There is one or more parameters missing\n"
+                + "HalpMi: Invalid format. Please follow the below example and try again.\n"
+                + "        add medicine /info Paracetamol,500,2023-12-12,Headaches,10\n",outContent.toString());
     }
 
     @Test
     public void deleteMedicineTest() {
         command.deleteMedicine(medicines,"1");
-        assertEquals("HalpMi: Please input a positive number up to 0 only.\r\n"
-                + "        Here is an example:\r\n" + "        delete patient /info 1\r\n",outContent.toString());
+        assertEquals("HalpMi: Please input a positive number up to 0 only.\n"
+                + "        Here is an example:\n" + "        delete patient /info 1\n",outContent.toString());
     }
 
 
