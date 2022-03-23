a3782905a5ffa274c8a758bd60642d1a5b781c7e
shxr3f <e0540641@u.nus.edu>
2022-03-23 17:04:22 +0800

    Resolving checkstyle issues in test files

--- src/test/java/seedu/duke/helper/CommandTest.java
+++ src/test/java/seedu/duke/helper/CommandTest.java
@@ -43,7 +43,7 @@ class CommandTest {
     }
 
     @Test
-    public void viewPatientTest(){
+    public void viewPatientTest() {
         command.viewPatient(patients,"S1234567A");
         assertEquals("HalpMi: There is no such patient\r\n",outContent.toString());
     }
@@ -53,7 +53,7 @@ class CommandTest {
         String userInput = "S1234567A";
         command.deletePatient(patients,userInput);
         assertEquals("HalpMi: Please input a positive number up to 0 only.\r\n"
-                +"        Here is an example:\r\n" + "        delete patient /info 1\r\n", outContent.toString());
+                + "        Here is an example:\r\n" + "        delete patient /info 1\r\n", outContent.toString());
     }
 
     @Test
@@ -66,24 +66,24 @@ class CommandTest {
     @Test
     public void deleteDoctorTest() {
         command.deleteDoctor(doctors,"");
-        assertEquals("HalpMi: Please input a positive number up to 0 only.\r\n" +
-                "        Here is an example:\r\n" + "        delete doctor /info 1\r\n",outContent.toString());
+        assertEquals("HalpMi: Please input a positive number up to 0 only.\r\n"
+                + "        Here is an example:\r\n" + "        delete doctor /info 1\r\n",outContent.toString());
     }
 
     @Test
     public void addMedicineTest() {
         String userInput = "Paracetamol";
         command.addMedicine(medicines,userInput);
-        assertEquals("HalpMi: There is one or more parameters missing\r\n" +
-                "HalpMi: Invalid format. Please follow the below example and try again.\r\n" +
-                "        add medicine /info Paracetamol,500,2023-12-12,Headaches,10\r\n",outContent.toString());
+        assertEquals("HalpMi: There is one or more parameters missing\r\n"
+                + "HalpMi: Invalid format. Please follow the below example and try again.\r\n"
+                + "        add medicine /info Paracetamol,500,2023-12-12,Headaches,10\r\n",outContent.toString());
     }
 
     @Test
     public void deleteMedicineTest() {
         command.deleteMedicine(medicines,"1");
-        assertEquals("HalpMi: Please input a positive number up to 0 only.\r\n" +
-                "        Here is an example:\r\n" + "        delete patient /info 1\r\n",outContent.toString());
+        assertEquals("HalpMi: Please input a positive number up to 0 only.\r\n"
+                + "        Here is an example:\r\n" + "        delete patient /info 1\r\n",outContent.toString());
     }
 
 
